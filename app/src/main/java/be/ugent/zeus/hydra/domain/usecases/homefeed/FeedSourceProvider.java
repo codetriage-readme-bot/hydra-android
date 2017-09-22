6cd4031fdecee9d00e013d6a1a3afbd131326f16
Niko Strijbol <strijbol.niko@gmail.com>
2017-09-22 22:24:31 +0200

    Add some documentation

--- app/src/main/java/be/ugent/zeus/hydra/domain/usecases/homefeed/FeedSourceProvider.java
+++ app/src/main/java/be/ugent/zeus/hydra/domain/usecases/homefeed/FeedSourceProvider.java
@@ -8,6 +8,17 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Provides the sources for the {@link GetHomeFeed} use case.
+ *
+ * While this class makes it possible to filter sources, this is not recommended, because the home feed itself does
+ * not remove cards when a source is deleted. Therefor it is better to have the source return an empty list when
+ * disabled.
+ *
+ * An example of this issue: image a home feed with card types 1, 2 and 3. The home feed has been loaded. However,
+ * due to some reason, card type 3 is no longer active. If you filter in this class, and trigger a refresh, the existing
+ * cards of type 3 will stay in the feed. When adding the source but having the source return an empty list, existing
+ * cards of type 3 will be removed from the feed.
+ *
  * @author Niko Strijbol
  */
 @Singleton
